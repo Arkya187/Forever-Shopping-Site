430aef02020f120431e1bff2ba31c7588262e3f1
Arkya187 <arkyachatterjee8@gmail.com>
2025-08-04 13:16:03 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -15,11 +15,7 @@ connectCloudinary()
 
 //middleware
 app.use(express.json())
-app.use(cors({
-  origin: ['https://forever-frontend-livid.vercel.app/'], // ✅ allow frontend origin
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+app.use(cors());
 
 //api endpoints
 
